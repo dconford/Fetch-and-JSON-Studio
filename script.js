75f6df9c8744409e85bde2a519e8c57d691a6d29
Daniel C <daniel@daniel.com>
2019-10-18 23:24:10 -0500

    finished bonus style items, still need sort.

--- script.js
+++ script.js
@@ -5,25 +5,42 @@
             // Access the JSON in the response
             response.json().then(function(json) {
               const div = document.getElementById("container");
-              let holdArray = []
+              let holder = '';
+              let holdArray = [];
               for (let i = 0; i < json.length; i++){
-                  let holder = `
-                  <div class="astronaut">
-                    <div class="bio">
-                        <h3>${json[i].firstName} ${json[i].lastName} </h3>
-                        <ul>
-                            <li>Hours in space: ${json[i].hoursInSpace}</li>
-                            <li>Active: ${json[i].active} </li>
-                            <li>Skills: ${json[i].skills} </li>
-                        </ul>
+                  if (json[i].active === true) {
+                    console.log("json[i].active is true (x2)", json[i].active);
+                    holder = `
+                    <div class="astronaut">
+                      <div class="bio">
+                          <h3>${json[i].firstName} ${json[i].lastName} </h3>
+                          <ul>
+                              <li>Hours in space: ${json[i].hoursInSpace}</li>
+                              <li style="color:green">Active: ${json[i].active} </li>
+                              <li>Skills: ${json[i].skills} </li>
+                          </ul>
+                      </div>
+                      <img class="avatar" src="${json[i].picture}">
                     </div>
-                    <img class="avatar" src="${json[i].picture}">
-                  </div>
-                  `;
+                    `;
+                  } else {
+                    holder = `
+                    <div class="astronaut">
+                        <div class="bio">
+                            <h3>${json[i].firstName} ${json[i].lastName} </h3>
+                            <ul>
+                                <li>Hours in space: ${json[i].hoursInSpace}</li>
+                                <li>Active: ${json[i].active} </li>
+                                <li>Skills: ${json[i].skills} </li>
+                            </ul>
+                        </div>
+                        <img class="avatar" src="${json[i].picture}">
+                    </div>
+                    `;
+                  }
                 holdArray.push(holder);
-                //rawDataHolder.push(tempHolder); 
               }
-              div.innerHTML = holdArray;
+              div.innerHTML = holdArray + `<h2 style="color:blue">Total # of Astronauts = ${json.length}`;
               });
             });
         });
