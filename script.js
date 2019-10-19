f144b8e00d147033faeaad4f87581d6d42fe3cdd
Daniel C <daniel@daniel.com>
2019-10-18 22:17:16 -0500

    fixed loop, still need bonus parts.

--- script.js
+++ script.js
@@ -5,22 +5,29 @@
             // Access the JSON in the response
             response.json().then(function(json) {
               const div = document.getElementById("container");
-              div.innerHTML = `
-              <div class="astronaut">
-                <div class="bio">
-                    <h3>${json[0].firstName} ${json[0].lastName} </h3>
-                    <ul>
-                        <li>Hours in space: ${json[0].hoursInSpace}</li>
-                        <li>Active: ${json[0].active} </li>
-                        <li>Skills: ${json[0].skills} </li>
-                    </ul>
-                </div>
-                <img class="avatar" src="${json[0].picture}">
-              </div>
-              `;
+              let holdArray = []
+              for (let i = 0; i < json.length; i++){
+                  let holder = `
+                  <div class="astronaut">
+                    <div class="bio">
+                        <h3>${json[i].firstName} ${json[i].lastName} </h3>
+                        <ul>
+                            <li>Hours in space: ${json[i].hoursInSpace}</li>
+                            <li>Active: ${json[i].active} </li>
+                            <li>Skills: ${json[i].skills} </li>
+                        </ul>
+                    </div>
+                    <img class="avatar" src="${json[i].picture}">
+                  </div>
+                  `;
+                holdArray.push(holder);
+                //rawDataHolder.push(tempHolder); 
+              }
+              div.innerHTML = holdArray;
+              });
             });
         });
-    });
+
 
 
 
